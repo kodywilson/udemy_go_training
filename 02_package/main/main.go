597c63f85741d3c20a98f02bfbb75bf1ab037d78
Kody Wilson <Kody.Wilson@nordstrom.com>
2018-01-15 09:21:32 -0800

    finished 02_package

---
+++ 02_package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kodywilson/udemy_go_training/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
