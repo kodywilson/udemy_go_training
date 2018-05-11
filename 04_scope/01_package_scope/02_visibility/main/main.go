41c3345d4f3d1969ec21d543e2b95a7932bacbd5
Kody Wilson <Kody.Wilson@nordstrom.com>
2018-05-11 14:34:03 -0700

    package level scope

---
+++ 04_scope/01_package_scope/02_visibility/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kodywilson/udemy_go_training/04_scope/01_package_scope/02_visibility/vis"
+)
+
+func main() {
+	fmt.Println(vis.MyName)
+	vis.PrintVar()
+}
---
+++ 04_scope/01_package_scope/02_visibility/vis/name.go
@@ -0,0 +1,4 @@
+package vis
+
+var MyName = "Kody"
+var yourName = "Future Rock Star Programmer"
