f38c323491851a64ff46f7a5535f4afdf22e3385
Kody Wilson <Kody.Wilson@nordstrom.com>
2018-06-01 12:10:58 -0700

    finish runes

--- section6_control-flow/10_for-loop/07_rune/02/main.go
+++ section6_control-flow/10_for-loop/07_rune/02/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	for i := 50; i <= 140; i++ {
-		fmt.Println(i, " - ", string(i), []byte(string(i)))
+		fmt.Println(i, " - ", string(i), " - ", []byte(string(i)))
 	}
 	foo := 'a'
 	fmt.Println(foo)
