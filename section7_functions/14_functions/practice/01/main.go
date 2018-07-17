ad8ea2169eaf5adfddd16371bdd8f280e0b969e8
kodywilson <kodywilson@gmail.com>
2018-07-17 15:09:02 -0700

    practice programs from memory during meetings

--- section7_functions/14_functions/practice/01/main.go
+++ section7_functions/14_functions/practice/01/main.go
@@ -3,17 +3,27 @@ package main
 import "fmt"
 
 func calc(x, y int) int {
-  return x + y
+	return x + y
 }
 
 func main() {
-  fmt.Println("This is only a test.")
-  x := 100
-  y := 63
-  fmt.Println("Jk, let's add some numbers.")
-  fmt.Println(x, "+", y, "=", calc(x, y))
-  fmt.Println("Hello!")
-  for i := 0; i < 10; i++ {
-    fmt.Println("Bye!")
-  }
+	fmt.Println("This is only a test.")
+	fmt.Println("Jk, let's add some numbers.")
+	var x int
+	fmt.Println("Please enter a whole number (no decimals): ")
+	_, err := fmt.Scanf("%d", &x)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var y int
+	fmt.Println("Please enter a whole number (no decimals): ")
+	_, err = fmt.Scanf("%d", &y)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(x, "+", y, "=", calc(x, y))
+	fmt.Println("Hello!")
+	for i := 0; i < 3; i++ {
+		fmt.Println("Bye!")
+	}
 }
