4ab73fef6f05ba1e62e308e0478ed997c340bbd7
kodywilson <kodywilson@gmail.com>
2018-07-30 13:26:52 -0700

    Completed functions exercise 3

--- section7_functions/16_exercises/03_greatest/main.go
+++ section7_functions/16_exercises/03_greatest/main.go
@@ -2,16 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
+func greatest(numbers []int) int {
 	var n, biggest int
-	numbers := []int{43, 56, 87, 12, 45, 57, 103, 22, 9}
-
 	for _, v := range numbers {
 		if v > n {
 			n = v
 			biggest = n
 		}
 	}
+	return biggest
+}
 
-	fmt.Println("The biggest number is", biggest)
+func main() {
+	numbers := []int{43, 56, 87, 12, 45, 57, 103, 22, 9}
+	fmt.Println("The biggest number is", greatest(numbers))
 }
