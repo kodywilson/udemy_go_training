ef8036ceae521b6aac80c292de4783f0579fa551
kodywilson <kodywilson@gmail.com>
2018-08-02 11:00:06 -0700

    correcting exercise 4 functions

--- section7_functions/16_exercises/03_greatest/main.go
+++ section7_functions/16_exercises/03_greatest/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func greatest(numbers []int) int {
+func greatest(numbers ...int) int {
 	var n, biggest int
 	for _, v := range numbers {
 		if v > n {
@@ -14,6 +14,6 @@ func greatest(numbers []int) int {
 }
 
 func main() {
-	numbers := []int{43, 56, 87, 12, 45, 57, 103, 22, 9}
-	fmt.Println("The biggest number is", greatest(numbers))
+	greatest := greatest(43, 56, 87, 12, 45, 57, 103, 22, 9)
+	fmt.Println("The biggest number is", greatest)
 }
