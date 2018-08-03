d8f8595a46c03cec457a900e611464c05b74f91b
kodywilson <kodywilson@gmail.com>
2018-08-03 10:56:03 -0700

    more array examples

--- section8_array/01/main.go
+++ section8_array/01/main.go
@@ -3,14 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var x [58]string
+	var x [58]int
 	fmt.Println(x)
 	fmt.Println(len(x))
 	fmt.Println(x[42])
-	for i := 65; i <= 122; i++ {
-		x[i-65] = string(i)
-	}
-	fmt.Println(x)
-	fmt.Println(len(x))
+	x[42] = 777
 	fmt.Println(x[42])
 }
