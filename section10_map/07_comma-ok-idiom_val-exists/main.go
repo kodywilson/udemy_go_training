30c2431a9000d0985f000e84db0042453a903b49
kodywilson <kodywilson@gmail.com>
2018-08-17 14:39:15 -0700

    more checking for val

--- section10_map/07_comma-ok-idiom_val-exists/main.go
+++ section10_map/07_comma-ok-idiom_val-exists/main.go
@@ -15,6 +15,18 @@ func main() {
 
 	if val, exists := myGreeting[2]; exists {
 		delete(myGreeting, 2)
+		fmt.Println("That value exists.")
+		fmt.Println("val: ", val)
+		fmt.Println("exists: ", exists)
+	} else {
+		fmt.Println("That value doesn't exist.")
+		fmt.Println("val: ", val)
+		fmt.Println("exists: ", exists)
+	}
+
+	if val, exists := myGreeting[2]; exists {
+		delete(myGreeting, 2)
+		fmt.Println("That value exists.")
 		fmt.Println("val: ", val)
 		fmt.Println("exists: ", exists)
 	} else {
