42e364e8311f5f64c2ef14c2728ff5543b906faa
Kody Wilson <kodywilson@gmail.com>
2018-08-31 16:38:11 -0700

    more map ranges

--- section10_map/10_loop-range/main.go
+++ section10_map/10_loop-range/main.go
@@ -15,5 +15,18 @@ func main() {
 	for key, val := range myGreeting {
 		fmt.Println(key, " - ", val)
 	}
-
+	var myNumbers = map[int]int{}
+	y := 10
+	for x := 0; x < 11; x++ {
+		myNumbers[x] = y
+		y--
+	}
+	for key, val := range myNumbers {
+		if key%2 == 0 {
+			fmt.Println(key, "is even and", val, "probably is too!")
+		}
+		if key%2 != 0 {
+			fmt.Println(key, "is odd and", val, "probably is too!")
+		}
+	}
 }
