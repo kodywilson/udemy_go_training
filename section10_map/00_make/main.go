5259d4eecb5f52775e65ada8808615964627a2dd
kodywilson <kodywilson@gmail.com>
2018-09-03 22:34:23 -0700

    using make to make a map

--- section10_map/00_make/main.go
+++ section10_map/00_make/main.go
@@ -4,9 +4,15 @@ import "fmt"
 
 func main() {
 
-  var myGreeting = make(map[string]string)
-  myGreeting["Terry"] = "Hola!"
-  myGreeting["Ernesto"] = "Good morning!"
+	var myGreeting = make(map[string]string)
+	myGreeting["Terry"] = "Hola!"
+	myGreeting["Ernesto"] = "Good morning!"
 
-  fmt.Println(myGreeting)
+	fmt.Println(myGreeting)
+
+	var myNumbers = make(map[int]int)
+	for i := 0; i < 11; i++ {
+		myNumbers[i] = i
+	}
+	fmt.Println(myNumbers)
 }
