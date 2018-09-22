dbea58c9f6d6ad611a8c019842125f1f8399fb33
Kody Wilson <kodywilson@gmail.com>
2018-09-22 07:17:43 -0700

    another function in my struct

--- section11_struct/03_methods/main.go
+++ section11_struct/03_methods/main.go
@@ -12,11 +12,16 @@ func (p person) fullName() string {
 	return p.first + p.last
 }
 
+func (p person) reverseName() string {
+	return p.last + p.first
+}
+
 func main() {
 	p1 := person{"James", "Bond", 20}
 	p2 := person{"Miss", "Moneypenny", 18}
 	fmt.Println(p1.first, p1.last, p1.age)
 	fmt.Println(p1.fullName())
+	fmt.Println(p1.reverseName())
 	fmt.Println(p2.first, p2.last, p2.age)
 	fmt.Println(p2.fullName())
 }
