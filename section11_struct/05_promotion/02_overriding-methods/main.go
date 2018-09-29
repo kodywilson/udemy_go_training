add712031fb89d29bb6b979aaf590d0d937b2006
Kody Wilson <kodywilson@gmail.com>
2018-09-29 09:12:07 -0700

    adding goldfinger to the conversation

--- section11_struct/05_promotion/02_overriding-methods/main.go
+++ section11_struct/05_promotion/02_overriding-methods/main.go
@@ -12,12 +12,20 @@ type DoubleZero struct {
 	LicenseToKill bool
 }
 
+type GoldFinger struct {
+	Person
+}
+
 func (p Person) Greeting() {
 	fmt.Println("I'm just a regular person.")
 }
 
 func (dz DoubleZero) Greeting() {
 	fmt.Println("Miss Moneypenny, so good to see you.")
+}
+
+func (gf GoldFinger) Greeting() {
+	fmt.Println("No, Mr. Bond, I expect you to die!")
 }
 
 func main() {
@@ -34,7 +42,15 @@ func main() {
 		LicenseToKill: true,
 	}
 
+	p3 := GoldFinger{
+		Person: Person{
+			Name: "Auric Goldfinger",
+			Age: 45,
+		},
+	}
+
 	p1.Greeting()
 	p2.Greeting()
 	p2.Person.Greeting()
+	p3.Greeting()
 }
