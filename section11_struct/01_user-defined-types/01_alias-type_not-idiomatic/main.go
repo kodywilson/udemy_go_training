cc3f1c1ad2270010dcec86a4da3ee9294c0b4e69
Kody Wilson <kodywilson@gmail.com>
2018-10-09 21:31:39 -0700

    adding bar

--- section11_struct/01_user-defined-types/01_alias-type_not-idiomatic/main.go
+++ section11_struct/01_user-defined-types/01_alias-type_not-idiomatic/main.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 type foo int
+type bar string
 
 func main() {
 	var myAge foo
 	myAge = 44
 	fmt.Printf("%T %v \n", myAge, myAge)
+	var myName bar
+	myName = "Kody"
+	fmt.Printf("%T %v \n", myName, myName)
 }
