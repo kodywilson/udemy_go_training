fd4e9d15d1173e83d2439f78d97a79329c9591eb
kodywilson <kodywilson@gmail.com>
2018-10-13 06:50:36 -0700

    more money penny

--- section11_struct/07_marshal-unmarshal/01_marshal/01_exported/main.go
+++ section11_struct/07_marshal-unmarshal/01_marshal/01_exported/main.go
@@ -13,9 +13,14 @@ type Person struct {
 }
 
 func main() {
-	p1 := Person{"James", "Bond", 20, 007}
+	p1 := Person{"James", "Bond", 32, 007}
 	bs, _ := json.Marshal(p1)
 	fmt.Println(bs)
 	fmt.Printf("%T \n", bs)
 	fmt.Println(string(bs))
+	p2 := Person{"Money", "Penny", 24, 000}
+	bs2, _ := json.Marshal(p2)
+	fmt.Println(bs2)
+	fmt.Printf("%T \n", bs2)
+	fmt.Println(string(bs2))
 }
