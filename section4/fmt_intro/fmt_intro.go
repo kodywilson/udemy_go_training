013c975013f527a33b70b054dcf01c6b21855ef6
Kody Wilson <kodywilson@gmail.com>
2019-09-25 16:36:53 -0700

    finish up fmt intro

--- section4/fmt_intro/fmt_intro.go
+++ section4/fmt_intro/fmt_intro.go
@@ -8,4 +8,10 @@ func main() {
 	fmt.Println(x)
 	fmt.Println("is of type")
 	fmt.Printf("%T\n", x)
+	fmt.Printf("The value of x is : ")
+	fmt.Printf("%v\n", x)
+	fmt.Println("Tab\tTab\tTab")
+
+	y := fmt.Sprintf("%v\t%v\t%v\t", x, x, x)
+	fmt.Println(y)
 }
