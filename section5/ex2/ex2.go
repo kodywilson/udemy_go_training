673b94b512da9ba2658ab6472ced6329ac4ce450
Kody Wilson <kodywilson@gmail.com>
2019-10-28 11:58:37 -0700

    complete section 5 exercise 2

---
+++ section5/ex2/ex2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println(x) //default is 0
+	fmt.Println(y) //default is a blank string
+	fmt.Println(z) //default is false
+}
